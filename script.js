b9910b7be1a007e835d0a78a367f88107fe29e5b
Goktug Erol <ge@avalanchedata.org>
2022-12-17 01:24:57 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function removeelement(){
+    document.getElementById("imgbox1").style.display="none";
+}
+function changeVisibility(){
+    document.getElementById("imgbox2").style.visibility="hidden";
+}
+function resetelement(){
+    document.getElementById("imgbox1").style.display="inline-block";
+    document.getElementById("imgbox2").style.visibility="visible";
+}
